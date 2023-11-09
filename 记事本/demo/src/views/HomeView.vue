26dace826392a2fd6984d2e69970cb0d5c416588
lchaof <2640371952@qq.com>
2023-11-09 16:52:59 +0800

    add

--- 记事本/demo/src/views/HomeView.vue
+++ 记事本/demo/src/views/HomeView.vue
@@ -4,18 +4,43 @@
       <img src="../../public/img.png" alt="">
       <h3 style="text-align: center;">~今天我需要做的事情~</h3>
       <div style="text-align: center; margin-top: 20px;">
-        <input type="text" placeholder="请输入未完成的list" style="padding: 5px;">
+        <input type="text" placeholder="请输入未完成的list" style="padding: 5px;" v-model="name">
         <button style="margin-left: 20px;" @click="add">添加</button>
       </div>
-      <div class="list">
 
-
-      </div>
-      <ul>
-        <li :class="gl == index ? 'dj' : ''" v-for="item, index in fl" :key="index" @click="qh(index)">{{ item.name }}
+      <ul class="tabul">
+        <li :class="gl == item.name ? 'dj' : ''" v-for="item, index in fl" :key="index" @click="qh(item.name)">{{
+          item.name }}
         </li>
 
       </ul>
+      <div class="list">
+        <!-- 全部数组 -->
+        <ul class="listul" v-show="gl == '查看所有'">
+          <li v-for="item, index in alllist" :key="index">
+            <p><input type="checkbox" v-model="item.flag" @click="allche(index)"></p>
+            <p v-show="item.show == 3"><s>{{ item.text }}</s></p>
+            <p v-show="item.show!=3">{{ item.text }}</p>
+            <p class="del" @click="del(index)">x</p>
+          </li>
+        </ul>
+        <!-- 待完成数组-->
+        <ul class="listul" v-show="gl == '待完成'">
+          <li v-for="item, index in dwclist" :key="index">
+            <p><input type="checkbox" name="" id="" v-model="item.flag" @click="dwcche(item.text)"></p>
+            <p>{{ item.text }}</p>
+            <p class="del" @click="dwcdel(item.text)">x</p>
+          </li>
+        </ul>
+        <!-- 已完成数组 -->
+        <ul class="listul" v-show="gl == '已完成'">
+          <li v-for="item, index in wclist" :key="index">
+            <p><input type="checkbox" name="" id="" v-model="item.flag" @click="ywcche(item.text)"></p>
+            <p><s>{{ item.text }}</s></p>
+            <p class="del" @click="wcdel(item.text)">x</p>
+          </li>
+        </ul>
+      </div>
 
     </div>
 
@@ -26,7 +51,7 @@
 export default {
   data() {
     return {
-      gl: 1,
+      gl: '查看所有',
       fl: [
         {
           name: "无事项"
@@ -41,21 +66,94 @@ export default {
         }
 
       ],
-      list: [
-        { text: '吃饭', flag: false, show: 1 }
-      ]
+      alllist: [
+        { text: '吃饭', flag: false, show: 2 },
+        { text: '睡觉', flag: false, show: 2 },
+        { text: '哈哈哈', flag: true, show: 3 },
+        { text: '啦啦啦', flag: false, show: 2 },
+        { text: '嘎嘎嘎', flag: true, show: 3 },
+        { text: '滋滋滋', flag: true, show: 3 },
+        { text: '嘻嘻嘻', flag: true, show: 3 },
+      ],
+      dwclist: [],
+      wclist: [],
+      name: ''
+
     };
   },
   methods: {
-    qh(index) {
-      this.gl = index
-      if (index == 0) {
-
+    qh(name) {
+      this.xr()
+      this.gl = name
+      if (this.gl == '清空所有') {
+        this.alllist = []
+        this.xr()
       }
+
     },
     add() {
-
+      this.alllist.push({ text: this.name, flag: false, show: 1 })
+      this.xr()
     },
+    xr() {
+      this.dwclist = []
+      this.wclist = []
+      this.alllist.forEach(item => {
+        if (item.show == 2) {
+          this.dwclist.push(item)
+        } else if (item.show == 3) {
+          this.wclist.push(item)
+        }
+      })
+    },
+    alldel(index) {
+      this.alllist.splice(index, 1)
+      this.xr
+    },
+    dwcdel(name) {
+      let index = this.alllist.findIndex(item => item.text == name)
+      this.alllist.splice(index, 1)
+      this.xr()
+    },
+    wcdel(name) {
+      let index = this.alllist.findIndex(item => item.text == name)
+      this.alllist.splice(index, 1)
+      this.xr()
+    },
+    allche(index) {
+      if (this.alllist[index].flag) {
+        this.alllist[index].flag = false
+        this.alllist[index].show = 2
+      } else {
+        this.alllist[index].flag = true
+        this.alllist[index].show = 3
+      }
+      this.xr()
+    },
+    dwcche(name) {
+      let index = this.alllist.findIndex(item => item.text == name)
+      if (this.alllist[index].flag) {
+        this.alllist[index].flag = false
+        this.alllist[index].show = 2
+      } else {
+        this.alllist[index].flag = true
+        this.alllist[index].show = 3
+      }
+      this.xr()
+    },
+    ywcche(name) {
+      let index = this.alllist.findIndex(item => item.text == name)
+      if (this.alllist[index].flag) {
+        this.alllist[index].flag = false
+        this.alllist[index].show = 2
+      } else {
+        this.alllist[index].flag = true
+        this.alllist[index].show = 3
+      }
+      this.xr()
+    }
+
+
   },
   computed: {},
   filters: {},
@@ -76,6 +174,7 @@ export default {
 .box1 {
   padding: 20px;
   width: 400px;
+  height: 800px;
   background-color: #fff;
   border-radius: 10px;
 
@@ -91,16 +190,24 @@ export default {
     border: 1px solid #000;
     padding: 5px 10px 5px 10px;
     border-radius: 5px;
-    transform: rotate(5deg);
+    cursor: pointer;
+  }
+
+  button:hover {
+    // font-size: 20px;
+    transform: rotate(10deg);
+    font-size: 17px;
+
   }
 
   input {
     background-color: #fff;
     border: none;
     border-bottom: 2px dashed #ed7b52;
+    outline: none;
   }
 
-  ul {
+  .tabul {
     display: flex;
     justify-content: space-between;
     margin-top: 20px;
@@ -118,6 +225,28 @@ export default {
     background-color: #ed7b52;
     border-radius: 5px;
     color: #fff;
+  }
+}
+
+.list {
+  .listul {
+    margin-top: 10px;
+
+    li {
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      // background-color: #ed7b52;
+      // color: #fff;
+      padding: 5px 10px 5px 10px;
+      border-radius: 10px;
+      margin-bottom: 5px;
+
+
+      .del {
+        cursor: pointer;
+      }
+    }
   }
 }
 </style>
